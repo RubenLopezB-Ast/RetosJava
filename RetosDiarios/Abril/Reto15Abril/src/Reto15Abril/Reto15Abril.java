ff9b69a60577adcaa33fc8d583919cfe2409da9b
RubenLopezB-Ast <rlb09@hotmail.es>
2025-04-17 00:53:52 +0200

    Reto 15 Abril sin hacer

---
+++ RetosDiarios/Abril/Reto15Abril/src/Reto15Abril/Reto15Abril.java
@@ -0,0 +1,13 @@
+package Reto15Abril;
+//Resulta que nos piden que programemos un validador de contraseñas. Algo fácil para los alumnos
+//de Prometeo. Solo tenemos que decir si, una contraseña introducida por terminal es válida o no.
+//Para que una contraseña sea válida debe:
+//Tener al menos 8 caracteres.
+//Tener al menos una letra minúscula.
+//Tener al menos una letra mayúscula.
+//Tener al menos un número.
+//Tener al menos un símbolo especial  de entre los siguientes *, ?, !, ^, “, $.
+//No contener la palabra GIT, en ningún formato (ni GIT, ni git, ni gIt, ni giT, ni Git, ni GIt,
+// ni gIT, ni GiT). Odiamos Git.
+public class Reto15Abril {
+}
