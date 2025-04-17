e1a578a15f33a32b1ee00f0f68154113c481ee95
RubenLopezB-Ast <rlb09@hotmail.es>
2025-04-18 00:53:40 +0200

    Reto 15 abril

--- RetosDiarios/Abril/Reto15Abril/src/Reto15Abril/Reto15Abril.java
+++ RetosDiarios/Abril/Reto15Abril/src/Reto15Abril/Reto15Abril.java
@@ -9,5 +9,28 @@ package Reto15Abril;
 //Tener al menos un símbolo especial  de entre los siguientes *, ?, !, ^, “, $.
 //No contener la palabra GIT, en ningún formato (ni GIT, ni git, ni gIt, ni giT, ni Git, ni GIt,
 // ni gIT, ni GiT). Odiamos Git.
+import java.util.Scanner;
 public class Reto15Abril {
+    public static void main(String[] args) {
+        String contrasena;
+        Scanner validador = new Scanner(System.in);
+        System.out.println("Escribe la contraseña: ");
+        contrasena = validador.nextLine();
+        if(contrasena.length()<8){
+            System.out.println("Contraseña corta debe de tener al menos 8 caracteres");
+        }else if (!contrasena.matches(".*[a-z].*")){
+            System.out.println("Contraseña incorrecta pon alguna letra minúscula");
+        }else if(!contrasena.matches(".*[A-Z].*")){
+            System.out.println("Contraseña incorrecta pon alguna letra mayúscula");
+        }else if (!contrasena.matches(".*[0-9].*")){
+            System.out.println("Contraseña incorrecta pon algún número");
+        }else if (!contrasena.matches(".*[*?!^“$].*")){
+            System.out.println("Contraseña incorrecta pon algún caractér especial");
+        }else if (contrasena.toUpperCase().contains("GIT")){
+            System.out.println("Contraseña incorrecta no puede conter el término (GIT) de ninguna forma");
+        }else{
+            System.out.println("Contraseña correcta. TODO OK");
+        }
+
+    }
 }
